db01773423026ca90e8c0a7119035cae8c81674c
ixally <xallmean@gmail.com>
2025-03-09 17:38:29 +0700

    Update utils.py

--- AyiinXd/ayiin/utils.py
+++ AyiinXd/ayiin/utils.py
@@ -224,7 +224,7 @@ async def autobot():
             f"BERHASIL MEMBUAT ASSISTANT BOT KAMU DENGAN USERNAME @{username}")
         await bot.send_message(
             BOTLOG_CHATID,
-            "**restarting bot. renewal userbot just chat @lenjzeh!**",
+            "**currently restarting userbot, if there are any problems please contact @jPipis!**",
         )
         heroku_var["BOT_TOKEN"] = token
         heroku_var["BOT_USERNAME"] = f"@{username}"
