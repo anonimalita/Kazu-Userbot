a5d31113a9ce7e4c529a772528faf906801368e4
ixally <xallmean@gmail.com>
2025-03-09 15:06:26 +0700

    Update __init__.py

--- AyiinXd/__init__.py
+++ AyiinXd/__init__.py
@@ -224,13 +224,13 @@ ALIVE_TEKS_CUSTOM = os.environ.get(
 ALIVE_NAME = os.environ.get("ALIVE_NAME", "𝗜𝗫𝗔𝗟𝗟-Userbot")
 
 # Custom Emoji Alive
-ALIVE_EMOJI = os.environ.get("ALIVE_EMOJI", "✨")
+ALIVE_EMOJI = os.environ.get("ALIVE_EMOJI", "🔥")
 
 # Custom Emoji Alive
-INLINE_EMOJI = os.environ.get("INLINE_EMOJI", "✨")
+INLINE_EMOJI = os.environ.get("INLINE_EMOJI", "🔥")
 
 # Custom icon HELP
-ICON_HELP = os.environ.get("ICON_HELP", "✨")
+ICON_HELP = os.environ.get("ICON_HELP", "🔥")
 
 # Time & Date - Country and Time Zone
 COUNTRY = str(os.environ.get("COUNTRY", "ID"))
